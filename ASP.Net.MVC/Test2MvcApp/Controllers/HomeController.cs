7dd4077326737aa9eacb255ef3a261cd8308d1bb
iDobryDen <59179395+iDobryDen@users.noreply.github.com>
2024-05-20 06:30:00 +0400

    First start done!

---
+++ ASP.Net.MVC/Test2MvcApp/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Test2MvcApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        public IActionResult About() => View();
+    }
+}
+
--- ASP.Net.MVC/Test2MvcApp/Program.cs
+++ ASP.Net.MVC/Test2MvcApp/Program.cs
@@ -5,9 +5,15 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        // добавляем поддержку контроллеров с представлениями
+        builder.Services.AddControllersWithViews();
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        // устанавливаем сопоставление маршрутов с контроллерами
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
     }
